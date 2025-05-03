629e1f3945ed3593bc423480bb8de5f83cbc2f30
Bobby Orphé HOUESSINON <orphe.h00@gmail.com>
2025-05-03 13:45:38 +0100

    add fontawesome kit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,45 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+	variable: "--font-geist-sans",
+	subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+	variable: "--font-geist-mono",
+	subsets: ["latin"],
 });
 
 // TODO: import SF Text and SF Icons
 
 export const metadata: Metadata = {
-  title: "iPhone 16 Pro et iPhone 16 Pro Max - Clone (FR)",
-  description: "iPhone 16 Pro et iPhone 16 Pro Max. Design en titane résistant. Commande de l’appareil photo. Dolby Vision 4K à 120 i/s. Puce A18 Pro.",
+	title: "iPhone 16 Pro et iPhone 16 Pro Max - Clone (FR)",
+	description:
+		"iPhone 16 Pro et iPhone 16 Pro Max. Design en titane résistant. Commande de l’appareil photo. Dolby Vision 4K à 120 i/s. Puce A18 Pro.",
 };
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="fr">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased font-[family-name:var(--font-geist-sans) font-[family-name:var(--font-geist-mono)`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="fr">
+			<head>
+				<Script
+					src="https://kit.fontawesome.com/064ddf1845.js"
+					crossOrigin="anonymous"
+					strategy="afterInteractive"
+				></Script>
+			</head>
+			<body
+				className={`${geistSans.variable} ${geistMono.variable} antialiased font-[family-name:var(--font-geist-sans) font-[family-name:var(--font-geist-mono) text-white`}
+			>
+				{children}
+			</body>
+		</html>
+	);
 }
--- app/ui/nav.tsx
+++ app/ui/nav.tsx
@@ -1,5 +1,7 @@
 export default function Nav() {
 	return (
-		<nav></nav>
+		<nav className="bg-black h-[48px]">
+			<div className="w-full max-w-5xl"></div>
+		</nav>
 	);
 }
