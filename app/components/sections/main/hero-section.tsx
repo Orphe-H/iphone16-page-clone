15fced9a24a41f72950f712f940980954c0c7ad4
Bobby Orphé HOUESSINON <orphe.h00@gmail.com>
2025-07-04 18:46:42 +0100

    add animation to first image

--- app/components/sections/main/hero-section.tsx
+++ app/components/sections/main/hero-section.tsx
@@ -1,10 +1,18 @@
+"use client";
+
 import Image from "next/image";
+import { motion } from "motion/react";
 
 export default function HeroSection() {
 	return (
 		<div className="bg-black py-12 xs:py-28 md:py-18 lg:py-0">
 			<div className="relative flex flex-col justify-center items-center">
-				<div className="relative w-full max-w-[433px] xs:max-w-[321px] md:max-w-[472px] lg:max-w-[703px] xl:max-w-[846px]">
+				<motion.div
+					className="relative w-full max-w-[433px] xs:max-w-[321px] md:max-w-[472px] lg:max-w-[703px] xl:max-w-[846px]"
+					initial={{ opacity: 0, scale: 1.2 }}
+					animate={{ opacity: 1, scale: 1 }}
+					transition={{ duration: 1.7, ease: "easeOut" }}
+				>
 					<Image
 						width={846}
 						height={352}
@@ -13,8 +21,8 @@ export default function HeroSection() {
 						className="w-full h-auto"
 						priority
 					/>
-				</div>
-				<div className="absolute top-1/4 inset-x-0 flex justify-center font-bold text-xl md:text-2xl lg:text-3xl z-20">
+				</motion.div>
+				<div className="absolute top-1/4 lg:top-24 xl:top-32 inset-x-0 flex justify-center font-bold text-xl md:text-2xl lg:text-3xl z-20">
 					iPhone 16 Pro
 				</div>
 			</div>
